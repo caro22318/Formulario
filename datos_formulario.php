db986fdef37c9da85370548b2d7aaf14ead0aba1
Victoria Real <victoriareall02@gmail.com>
2024-10-02 20:34:47 +0100

    Datos formulario VRL

---
+++ datos_formulario.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulario de Datos</title>
+    <style>
+        .error { color: #ff0000; }
+    </style>
+</head>
+<body>
+<form method="post" action="salida.php">
+
+    
+    <b>Nombre</b><br><input type="text" name="name" placeholder="Ingrese su nombre"><br>
+    <br><b>Correo electrónico</b> <br> <input type="text" name="email" placeholder="Ingrese un correo válido"><br>
+    <br><b>Sitio Web</b> <br><input type="text" name="web" placeholder="URL"><br>
+   
+    <br><textarea name="comment" placeholder= "Inserte un comentario.." rows="5" cols="40"></textarea><br>
+
+    <b>Género</b><br>
+    <input type="radio" name="gender" value="Mujer">Mujer
+    <input type="radio" name="gender" value="Hombre">Hombre
+    <input type="radio" name="gender" value="Otro">Otro<br><br>
+
+    <b><label for ="devices">Dispositivos en propiedad:</label><b><br>
+    <select id ="devices" name="devices[]" multiple>
+        <option value="Tablet" selected>Tablet</option><br>
+        <option value="Ordenador de mesa" selected>Ordenador de mesa</option><br>
+        <option value="Portátil" selected>Portátil</option>
+        <option value="Consola" selected>Consola</option>
+    </select>
+    <br><br><label for="aviso">¿Quieres que te llegue un correo con la recogida de datos?</label>
+    <input type ="checkbox" id="aviso" name="aviso" check><br>
+
+    <br><input type="submit" name="enviar" value="Enviar">
+    </form>
+</body>
+</html>
