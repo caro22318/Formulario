f134c261333a418e241cf98a3fbdad9a6c5d2d28
Victoria Real <victoriareall02@gmail.com>
2024-10-03 22:18:33 +0100

    cambios

--- datos_formulario.php
+++ datos_formulario.php
@@ -8,7 +8,7 @@
     </style>
 </head>
 <body>
-<form method="post" action="salida.php" enctype="multipart/form-data"> <?php //el multipart permite compilar un conjunto de pares clave/valor?>
+<form method="post" action="recibe_datosV.php" enctype="multipart/form-data"> <?php //el multipart permite compilar un conjunto de pares clave/valor?>
         <?php 
         $archivoFile = fopen("victoria_archivo.txt", "r") or die ("No se pudo abrir el archivo");
         $name = null;
@@ -45,6 +45,14 @@
     </select>
     <br><br><label for="aviso">¿Quieres que te llegue un correo con la recogida de datos?</label>
     <input type ="checkbox" id="aviso" name="aviso" check><br>
+
+    <br><label for ="fichero1">Subir archivo 1:</label>
+    <input type ="file" name ="fichero1" id="fichero1"><br>
+
+    <label for="fichero2">Subir archivo 2:</label>
+    <input type="file" name ="fichero2" id="fichero2"><br>
+    
+    <input type="hidden" name="formulario" value="datos_formulario">
 
     <br><input type="submit" name="enviar" value="Enviar">
     </form>
